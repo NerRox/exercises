6989a0b289a999ba3cdaf097165b74e1ce419e05
NerRox <alex_taylor32@aol.com>
2017-10-10 07:31:52 +0300

    Some adds

---
+++ Codewars/squareEveryDigit.js
@@ -0,0 +1,21 @@
+/*Welcome. In this kata, you are asked to square every digit of a number.
+
+For example, if we run 9119 through the function, 811181 will come out.
+
+Note: The function accepts an integer and returns an integer
+*/
+
+
+function squareDigits(num){
+	let x = String(num)
+	let y = x.split('')
+	let u = []
+	for (let i = 0; i < y.length; i++) {
+		u.push(Math.pow(y[i], 2))
+	}
+
+	let o = u.join('')
+	return +o
+}
+
+squareDigits(8118) // 641164
