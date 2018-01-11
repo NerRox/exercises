f5055f0bb7876d86316bb6d71031642b0d017651
NerRox <alex_taylor32@aol.com>
2018-01-11 06:23:34 +0300

    Some adds

---
+++ Codewars/guid.js
@@ -0,0 +1,39 @@
+function uid(len) {
+	return Math.floor((Math.random()) * 1e+30).toString(16).slice(0,len)
+}
+
+
+function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0
+		v = c == 'x' ? r : (r & 0x3 | 0x8)
+		return v.toString(16)
+	})
+}
+
+
+function uuidv42() {
+	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+	)
+}
+
+
+
+
+function vocUUID(len) {
+	let str = ""
+	let voc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	for (let i = 0; i < len; i++)
+		str += voc.charAt(Math.floor(Math.random() * voc.length))
+
+	return str
+}
+
+
+
+
+function randN(n){
+	return (Math.random()+'').slice(2, 2 + Math.max(1, Math.min(n, 15)) ).toString(16);
+}
